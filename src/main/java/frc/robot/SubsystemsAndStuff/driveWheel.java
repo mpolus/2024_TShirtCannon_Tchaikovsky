afd6877cdf81b282963d2085279c0f3f8c9aa626
sheltrob000 <sheltrob000@warren.k12.in.us>
2024-09-09 08:56:51 -0400

    start of new week

--- src/main/java/frc/robot/SubsystemsAndStuff/driveWheel.java
+++ src/main/java/frc/robot/SubsystemsAndStuff/driveWheel.java
@@ -1,11 +1,7 @@
 package frc.robot.SubsystemsAndStuff;
 
-
-
-
 public class driveWheel {
     private static final double motorToWheelRatio = 50; //placeholder please replace later
-    
 
     public driveWheel(int moduleNumber) {
         
--- src/main/java/frc/robot/SubsystemsAndStuff/steer.java
+++ src/main/java/frc/robot/SubsystemsAndStuff/steer.java
@@ -8,16 +8,36 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.ExponentialProfile.Constraints;
 
-/** Add your docs here. */
+
 public class steer {
 private static final MotorType motortype = MotorType.kBrushless;
-private static final double gearing = -150/7;
+private static final double gearing = -150.0/7.0;
+private static final double kS = 1;//placeholder change please!!!!!!!!!!!!
+private static final double kV = 0.427607143;
+private static final double kP = .001; //placeholder
+private static final double kI = 0;
+private static final double kD = .001; // placeholder
+
+
 CANSparkMax canSparkMax;
+
+private final TrapezoidProfile trapezoidProfile;
+private final PIDController pidController;
+private final SimpleMotorFeedforward simpleMotorFeedforward;
 
 
 public steer(int motuleNumber) {
     canSparkMax = new CANSparkMax(motuleNumber + 10, motortype);
+    simpleMotorFeedforward = new SimpleMotorFeedforward(kS, kV)
+    pidController = new PIDController(kP, kI, kD);
+    double maxVelocity = simpleMotorFeedforward.maxAchievableVelocity(12, 0);
+    double maxAccerlation = simpleMotorFeedforward.maxAchievableAcceleration(12, 0)
+    
 }
 
 public double getPositionDegrees() {
