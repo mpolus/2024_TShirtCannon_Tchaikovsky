d586c83f0e6c4ce5bbf216d25695688a08ccf02f
sheltrob000 <sheltrob000@warren.k12.in.us>
2024-09-11 13:58:08 -0400

    this

--- src/main/java/frc/robot/SubsystemsAndStuff/steer.java
+++ src/main/java/frc/robot/SubsystemsAndStuff/steer.java
@@ -11,74 +11,65 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
-
 
 public class steer {
-private static final MotorType motortype = MotorType.kBrushless;
-private static final double gearing = -150.0/7.0;
-private static final double kS = 1;//placeholder change please!!!!!!!!!!!!
-private static final double kV = 0.427607143;
-private static final double kP = .001; //placeholder
-private static final double kI = 0;
-private static final double kD = .001; // placeholder
-private static final double framerate = .2;
+    private static final MotorType motortype = MotorType.kBrushless;
+    private static final double gearing = -150.0 / 7.0;
+    private static final double kS = 1;// placeholder change please!!!!!!!!!!!!
+    private static final double kV = 0.427607143;
+    private static final double kP = .001; // placeholder
+    private static final double kI = 0;
+    private static final double kD = .001; // placeholder
+    private static final double framerate = .02;
 
+    CANSparkMax canSparkMax;
 
-CANSparkMax canSparkMax;
+    private final TrapezoidProfile trapezoidProfile;
+    private final PIDController pidController;
+    private final SimpleMotorFeedforward simpleMotorFeedforward;
 
-private final TrapezoidProfile trapezoidProfile;
-private final PIDController pidController;
-private final SimpleMotorFeedforward simpleMotorFeedforward;
+    public steer(int motuleNumber) {
+        canSparkMax = new CANSparkMax(motuleNumber + 10, motortype);
+        simpleMotorFeedforward = new SimpleMotorFeedforward(kS, kV);
+        pidController = new PIDController(kP, kI, kD);
+        double maxVelocity = simpleMotorFeedforward.maxAchievableVelocity(12, 0);
+        double maxAccerlation = simpleMotorFeedforward.maxAchievableAcceleration(12, 0);
+        trapezoidProfile = new TrapezoidProfile(new TrapezoidProfile.Constraints(maxVelocity, maxAccerlation));
 
+    }
 
-public steer(int motuleNumber) {
-    canSparkMax = new CANSparkMax(motuleNumber + 10, motortype);
-    simpleMotorFeedforward = new SimpleMotorFeedforward(kS, kV);
-    pidController = new PIDController(kP, kI, kD);
-    double maxVelocity = simpleMotorFeedforward.maxAchievableVelocity(12, 0);
-    double maxAccerlation = simpleMotorFeedforward.maxAchievableAcceleration(12, 0);
-    trapezoidProfile = new TrapezoidProfile(new TrapezoidProfile.Constraints(maxVelocity, maxAccerlation));
-    
-}
+    // woooo getters
 
+    public double getPositionDegrees() {
+        double degrees = canSparkMax.getEncoder().getPosition() * 360 / gearing;
+        degrees = MathUtil.inputModulus(degrees, 0, 360);
+        return degrees;
+    }
 
-// woooo getters
+    public double getVelocityDegreesPerSecond() {
+        double degreesPerSecond = canSparkMax.getEncoder().getVelocity() * 360 / 60 / gearing;
+        return degreesPerSecond;
+    }
 
-public double getPositionDegrees() {
-    double degrees = canSparkMax.getEncoder().getPosition() * 360 / gearing;
-    degrees = MathUtil.inputModulus(degrees, 0, 360);
-    return degrees;
-}
+    // under this is the monster of trying to set position.
 
-public double getVelocityDegreesPerSecond() {
-    double degreesPerSecond = canSparkMax.getEncoder().getVelocity() * 360 / 60 / gearing;
-    return degreesPerSecond;
-}
+    public void SetMotorToPosition(double positionSetpointDegrees) {
+        // gets current state
+        double currentPosition = getPositionDegrees();
+        double currentVelocity = getVelocityDegreesPerSecond();
+        // puts current state into a usable object and makes a goal state
+        var currentState = new TrapezoidProfile.State(currentPosition, currentVelocity);
+        var goalState = new TrapezoidProfile.State(positionSetpointDegrees, 0);
+        // create a usable goal state using above stuff
+        var profiledstate = trapezoidProfile.calculate(framerate, currentState, goalState);
 
+        // finds voltage to send to motor based off of stuff above.
+        var pidvoltage = pidController.calculate(currentVelocity, positionSetpointDegrees);
+        var motorffvoltage = simpleMotorFeedforward.calculate(currentVelocity, profiledstate.velocity, framerate);
+        var voltage = pidvoltage + motorffvoltage;
+        canSparkMax.setVoltage(voltage);
 
-// under this is the monster of trying to set position.
-
-
-public void SetMotorToPosition(double positionSetpointDegrees) {
-    double currentPosition = getPositionDegrees();
-    double currentVelocity = getVelocityDegreesPerSecond()
-    var currentState = new TrapezoidProfile.State(currentPosition, currentVelocity);
-    var goalState = new TrapezoidProfile.State(positionSetpointDegrees, 0);
-
-    trapezoidProfile.calculate(framerate, currentState, goalState);
-
-
-}
-
-
-
-
-
-
-
-
-
+    }
 
 }
 // using a neo
