07bff1ba47106ef125589cf05a32416b487cae35
sheltrob000 <sheltrob000@warren.k12.in.us>
2024-09-19 09:59:43 -0400

    day armstrong gone

--- src/main/java/frc/robot/SubsystemsAndStuff/driveWheel.java
+++ src/main/java/frc/robot/SubsystemsAndStuff/driveWheel.java
@@ -2,6 +2,7 @@ package frc.robot.SubsystemsAndStuff;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 
@@ -10,8 +11,17 @@ public class driveWheel {
     private static final double weelRadius = .2;//placeholder please replace later
     private static final double dtseconds = 0.02;
 
+    // wooo this chunk is all placeholder values!!!!!
+    private static final double kS = 1;
+    private static final double kV = 2.6158;
+    private static final double kA = 0.054006;
+    private static final double kP = .001;
+    private static final double kI = 0.0;
+    private static final double kD = 0.0;
+
     private final TrapezoidProfile trapezoidProfile;
     private final SimpleMotorFeedforward simpleMotorFeedforward;
+    private final PIDController pidController;
 
     private final TalonFX talonfx;
     
@@ -19,6 +29,13 @@ public class driveWheel {
 
     public driveWheel(int moduleNumber) {
         talonfx = new TalonFX(moduleNumber + 20);
+        simpleMotorFeedforward = new SimpleMotorFeedforward(kS, kV, kA);
+        pidController = new PIDController(kP, kI, kD);
+        double maxVelocity = simpleMotorFeedforward.maxAchievableVelocity(12, 0);
+        double maxAccerlation = simpleMotorFeedforward.maxAchievableAcceleration(12, 0);
+        var constraints = new TrapezoidProfile.Constraints(maxVelocity, maxAccerlation);
+        trapezoidProfile = new TrapezoidProfile(constraints);
+
     }
 
 
