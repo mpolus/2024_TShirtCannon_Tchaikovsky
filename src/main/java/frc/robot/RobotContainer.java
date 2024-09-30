3d0172168cd97c099636d65dfea14b9d143ebc03
Jacob <stephjac000@warren.k12.in.us>
2024-09-30 16:41:47 -0400

    updates

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,13 +1,15 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
+import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.LED;
 
 public class RobotContainer {
+
+  private final LED ledSubsystem = new LED(0, 70);
+  private final CommandXboxController controller = new CommandXboxController(0);
 
   public RobotContainer() {
     configureBindings();
@@ -15,10 +17,15 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
-
+    controller.a().onTrue(ledSubsystem.setColorCommand(new Color8Bit(0, 255, 0)));
+    controller.b().onTrue(ledSubsystem.setColorCommand(new Color8Bit(0, 128, 255)));
+    controller.x().whileTrue(ledSubsystem.setRainbow());
+    controller.x().onFalse(ledSubsystem.setColorCommand(new Color8Bit("#d4af37")));
   }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
   }
+
+
 }
