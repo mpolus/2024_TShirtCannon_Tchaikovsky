ca14a0f1626ceac4c6c0f2828d22eb1716426379
Jacob <stephjac000@warren.k12.in.us>
2024-10-28 16:26:57 -0400

    elevatorConfigDraftDone

--- src/main/java/com/mechanisms/elevators/ElevatorConfiguration.java
+++ src/main/java/com/mechanisms/elevators/ElevatorConfiguration.java
@@ -1,45 +1,35 @@
 package com.mechanisms.elevators;
 
 import com.control.ClosedLoopParameters;
+import com.control.position.ExponentialProfileParameters;
+import com.control.position.TrapezoidProfileParameters;
+import com.control.velocity.VelocityProfileParameters;
 
 public class ElevatorConfiguration {
 
-    /** The gear ratio (reduction) as a ratio of output rotations to input rotations. */
     public double Reduction;
 
-    /** The radius of the elevators drum in meters. */
-    // TODO: Jacob - Initialize a double.  It should be public and should be named drumRadiusMeters.
+    public double drumRadiusMeters;
 
-    /** The minimum position of the elevator in meters. */
-    // TODO: Jacob - Initialize a double.  It should be public and should be named MinPosition.
+    public double MinPosition;
+    
+    public double MaxPosition;
 
-    /** The maximum position of the elevator in meters. */
-    // TODO: Jacob - Initialize a double.  It should be public and should be named MaxPosition.
-
-    /** The ClosedLoopParameters for position control via voltage. */
     public ClosedLoopParameters PositionClosedLoopVoltageParams = new ClosedLoopParameters();
 
-    /** The ClosedLoopParameters for position control via current. */
-    // TODO: Jacob - Initialize a ClosedLoopParameters object.  It should be public and should be named PositionClosedLoopCurrentParams.
+    public ClosedLoopParameters PositionClosedLoopCurrentParams;
 
-    /** The ClosedLoopParameters for velocity control via voltage. */
-    // TODO: Jacob - Initialize a ClosedLoopParameters object.  It should be public and should be named VelocityClosedLoopVoltageParams.
+    public ClosedLoopParameters VelocityClosedLoopVoltageParams;
 
-    /** The OpenLoopParameters for this mechanism via voltage. */
-    // TODO: Jacob - Initialize a OpenLoopParameters object.  It should be public and should be named OpenLoopVoltageParams.
+    public ClosedLoopParameters OpenLoopVoltageParams;
 
-    /** The OpenLoopParameters for this mechanism via current. */
-    // TODO: Jacob - Initialize a OpenLoopParameters object.  It should be public and should be named OpenLoopCurrentParams.
+    public ClosedLoopParameters OpenLoopCurrentParams;
 
-    /** The trapezoid profile parameters. */
-    // TODO: Jacob - Initialize a TrapezoidProfileParameters object.  It should be public and named TrapProfileParams.
+    public TrapezoidProfileParameters TrapProfileParams;
 
-    /** The velocity profile parameters.*/
-    // TODO: Jacob - Initialize a VelocityProfileParameters object.  It should be public and named VelocityProfileParams.
+    public VelocityProfileParameters VelocityProfileParams;
 
-    /** The exponential profile parameters via voltage. */
-    // TODO: Jacob - Initialize an ExponentialProfileParameters object. It should be public and named ExpoProfileVoltageParams.
+    public ExponentialProfileParameters ExpoProfileVoltageParams;
 
-    /** The exponential profile parameters via current. */
-    // TODO: Jacob - Initialize an ExponentialProfileParameters object. It should be public and named ExpoProfileCurrentParams.
+    public ExponentialProfileParameters ExpoProfileCurrentParams;
 }
